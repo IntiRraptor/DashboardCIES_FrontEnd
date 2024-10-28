053513e808dff2389eed4b1c7dd73471b6de2768
IntiRraptor <99521721+IntiRraptor@users.noreply.github.com>
2024-10-28 17:55:48 -0400

    #AddNewCredentials

--- src/lib/auth-credentials.ts
+++ src/lib/auth-credentials.ts
@@ -5,4 +5,7 @@ export interface Credential {
 
 export const credentials: Credential[] = [
   { email: "admin@admin.com", password: "admin" },
+  { email: "jlinares@cies.org.bo", password: "x7Y2z9" },
+  { email: "acabezas@cies.org.bo", password: "m3N8p1" },
+  { email: "rballesteros@cies.org.bo", password: "q4R6s2" },
 ];
